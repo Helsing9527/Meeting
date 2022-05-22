cb76c638ee9e1b74e486e2177e96fb1de5a80da2
Social-Hex <msdn-ts@outlook.com>
2022-05-22 10:59:49 +0800

    5/22

--- src/main/java/com/scetop/meeting/controller/LoginSuccessController.java
+++ src/main/java/com/scetop/meeting/controller/LoginSuccessController.java
@@ -1,18 +1,16 @@
 package com.scetop.meeting.controller;
 
-import com.scetop.meeting.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginSuccessController {
-    @RequestMapping("/success")
-    public ModelAndView home(User user) {
-        System.out.println("success");
-        System.out.println(user);
+    @RequestMapping("/success/{id}")
+    public ModelAndView home(@PathVariable(name = "id") String id) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("user", user);
+        modelAndView.addObject("id", id);
         modelAndView.setViewName("redirect:/pages/home.html");
         return modelAndView;
     }
